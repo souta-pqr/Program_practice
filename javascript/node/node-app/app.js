64e32db3e216453b03e51e88b02e2044657953b1
Souta Kobori <s21c1048fl@s.chibakoudai.jp>
2023-06-12 16:53:09 +0900

    uses cookie

--- javascript/node/node-app/app.js
+++ javascript/node/node-app/app.js
@@ -43,58 +43,59 @@ function getFromClient(request, response) {
 
 var data = { msg: 'no message...' };
 
-// index access progress
+// indexのアクセス処理
 function response_index(request, response) {
-	//POST access progress
-	if (request.method == 'POST') {
-		var body = '';
+  // POSTアクセス時の処理
+  if (request.method == 'POST') {
+    var body = '';
 
-	//Data reception event handing
-	request.on('data', (data) => {
-		body += data;
-	});
+    // データ受信のイベント処理
+    request.on('data', (data) => {
+      body += data;
+    });
 
-	//Data reception end event procession
-		request.on('end', () => {
-			data = qs.parse(body);
-			write_index(request, response);
-		});
-	}
-	else {
-		write_index(request, response);
-	}
+    // データ受信終了のイベント処理
+    request.on('end', () => {
+      data = qs.parse(body);
+      // クッキーの保存
+      setCookie('msg', data.msg, response);
+      write_index(request, response);
+    });
+  } else {
+    write_index(request, response);
+  }
 }
 
+// indexのページ作成
 function write_index(request, response) {
-	var msg = "Display message.";
-	var content = ejs.render(index_page, {
-		title: "Index",	
-		content: msg,
-		data: data,
-	});
-	response.writeHead(200, { 'Content-Type': 'text/html' });
-	response.write(content);
-	response.end()
-}
-
-var data2 = {
-  'Taro': ['taro@yamada', '09-999-999', 'Tokyo'],
-  'Hanako': ['hanako@flower', '080-888-888', 'Yokohama'],
-  'Sachiko': ['sachi@happy', '070-777-777', 'Nagoya'],
-  'Ichiro': ['ichi@baseball', '060-666-666', 'USA'],
-}
-
-// otherのアクセス処理
-function response_other(request, response) {
-  var msg = "This is Other page."
-  var content = ejs.render(other_page, {
-    title: "Other",
+  var msg = "Display message"
+  var cookie_data = getCookie('msg', request);
+  var content = ejs.render(index_page, {
+    title: "Index",
     content: msg,
-    data: data2,
-    filename: 'data_item'
+    data: data,
+    cookie_data: cookie_data,
   });
   response.writeHead(200, { 'Content-Type': 'text/html' });
   response.write(content);
   response.end();
 }
 
+// クッキーの値を設定
+function setCookie(key, value, response) {
+  var cookie = escape(value);
+  response.setHeader('Set-Cookie', [key + '=' + cookie]);
+}
+// クッキーの値を取得
+function getCookie(key, request) {
+  var cookie_data = request.headers.cookie != undefined ?
+    request.headers.cookie : '';
+  var data = cookie_data.split(';');
+  for (var i in data) {
+    if (data[i].trim().startsWith(key + '=')) {
+      var result = data[i].trim().substring(key.length + 1);
+      return unescape(result);
+    }
+  }
+  return '';
+}
