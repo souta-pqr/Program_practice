b357ecb53a8930fe24b0a6a149ffd1b2bda53637
Souta Kobori <s21c1048fl@s.chibakoudai.jp>
2023-06-12 09:41:36 +0900

    make some webs

--- javascript/node/node-app/app5.js
+++ javascript/node/node-app/app5.js
@@ -2,7 +2,7 @@ const http = require('http');
 const fs = require('fs');
 const ejs = require('ejs');
 
-const index_page = fs.readFileSync('./index.ejs', 'utf8');
+const index_page = fs.readFileSync('./index1.ejs', 'utf8');
 
 var server = http.createServer(getFromClient);
 
@@ -13,7 +13,10 @@ console.log('Server start!');
 
 // createServer progress
 function getFromClient(request, response) {
-	var content = ejs.render(index_page);
+	var content = ejs.render(index_page, {
+		title: "Index page",
+		content: "This sample page is used template.",
+	});
 	response.writeHead(200, {'Content-Type': 'text/html'});
 	response.write(content);
 	response.end();
