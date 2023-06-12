1a45e94f7467c8ec7dd3e42e4e6f6d49c3ca8a9e
Souta Kobori <s21c1048fl@s.chibakoudai.jp>
2023-06-12 12:56:00 +0900

    form

--- javascript/node/node-app/app.js
+++ javascript/node/node-app/app.js
@@ -2,6 +2,7 @@ const http = require('http');
 const fs = require('fs');
 const ejs = require('ejs');
 const url = require('url');
+const qs = require('querystring');
 
 const index_page = fs.readFileSync('./index.ejs', 'utf-8');
 const other_page = fs.readFileSync('./other.ejs', 'utf-8');
@@ -20,23 +21,72 @@ function getFromClient(request, response) {
 	var url_parts = url.parse(request.url, true);
 	switch(url_parts.pathname) {
 		case '/':
-			var content = "This is Index page."
-			var query = url_parts.query;
-			if (query.msg != undefined) {
-				content += 'you [' + query.msg + '] send.';
-		}
-		var content = ejs.render(index_page, {
-			title: "Index",
-			content: content,
+			response_index(request, response);
+			break;
+	
+		case '/other':
+			response_other(request, response);
+			break;
+
+		case 'style.css':
+			response.writeHead(200, { 'Content-Type': 'text/html' });
+			response.write(style_css);
+			response.end();
+			break;
+
+		default:
+			response.writeHead(200, { 'Content-Type': 'text/plane' });
+			response.end('no page...');
+			break;
+	}
+}
+
+function response_index(request, response) {
+	var msg = "This is Index page."
+	var content = ejs.render(index_page, {
+		title: "Index",
+		content: msg,
+	});
+	response.writeHead(200, { 'Content-Type': 'text/html' });
+	response.write(content);
+	response.end();
+}
+
+function response_other(request, response) {
+	var msg = "This is Other page.";
+
+	// POST access progress
+	if (request.method == 'POST') {
+		var body = '';
+
+	// When Received data, event progress
+	request.on('data', (data) => {
+		body += data;
+	});
+
+	// When stopping Received data, event progress
+	request.on('end', () => {
+		var post_data = qs.parse(body);
+		msg += 'You [' + post_data.msg + '] wrote';
+		var content = ejs.render(other_page, {
+			title: "Other",
+			content: msg,
 		});
 		response.writeHead(200, { 'Content-Type': 'text/html' });
 		response.write(content);
 		response.end();
-		break;
+	});
 
-	default:
-		response.writeHead(200, { 'Content-Type': 'text/plane' });
-		response.end('no page...');
-		break;
+	// GET aceess progress
+	}
+	else {
+		var msg = "no page...";
+		var content = ejs.render(other_page, {
+			title: "Other",
+			content: msg,
+		});
+		response.writeHead(200, { 'Content-Type': 'text/html' });
+		response.write(content);
+		response.end();
 	}
 }
