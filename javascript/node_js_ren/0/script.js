081acec66d2b23af115b180cb084a8843b6b65fa
souta-pqr <boriko0116@gmail.com>
2023-12-09 10:01:51 +0900

    first

--- javascript/node_js_ren/0/script.js
+++ javascript/node_js_ren/0/script.js
@@ -4,3 +4,21 @@ document.write('<p>価格:' + price + '円</p>');
 const tax = 0.10;
 price = Math.floor(price * (1.0 + tax));
 document.write('<p>税込価格:' + price + '円</p>');
+
+function calc(max) {
+    var total = 0;
+    for (var i = 0; i < max; i++) {
+        total += i;
+    }
+    document.write('<p>' + max + 'までの合計は，' + total + '</p>');
+}
+
+calc(100)
+
+var arr = [100, 94, 202, 29];
+var total = 0
+for (var n in arr){
+    total += arr[n]
+}
+var average = total / arr.length
+document.write('<p>配列の合計値は' + total + '、平均値は' + average + '</p>')
