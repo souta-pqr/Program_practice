935add76000fe0f9c06f87e2f5b6de16ff17a196
souta-pqr <boriko0116@gmail.com>
2023-12-31 17:46:14 +0900

    2 lesson ans

---
+++ c/yasashi/2/sample2_2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("%cは文字です．\n", 'A');
+    printf("%dは整数です．\n", 123);
+    printf("%.2fは少数です。\n", 123.456);
+
+    return 0;
+}
---
+++ c/yasashi/2/sample2_3.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("%d\n", 123);
+    printf("\\%dもらった\n", 100);
+    printf("またあした\n");
+
+    return 0;
+}
