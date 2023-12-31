265adeec6dcf931c5880dc2931ee51e25b638510
souta-pqr <boriko0116@gmail.com>
2023-12-31 17:57:10 +0900

    2 lesson answer continuation

---
+++ c/yasashi/2/sample2_4.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void){
+    printf("1\t2\t3\n");
+
+    return 0;
+}
---
+++ c/yasashi/2/sample2_5.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void){
+    printf("%o\n%o\n%o\n", 6, 16, 11);
+    printf("%x\n%x\n%x\n", 6, 32, 19);
+
+    return 0;
+}
