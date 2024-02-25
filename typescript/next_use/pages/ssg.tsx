85f284a74c38068b994245bc3b1cb42370869c6e
souta-pqr <boriko0116@gmail.com>
2024-02-25 20:52:21 +0900

    SSR SSG sample code

--- typescript/next_use/pages/ssg.tsx
+++ typescript/next_use/pages/ssg.tsx
@@ -1,22 +1,39 @@
-import { NextPage } from "next";
+import { GetStaticProps, NextPage, NextPageContext } from "next";
 import Head from "next/head";
+import { experimental_taintObjectReference } from "react";
 
-type SSGProps = {}
+type SSGProps = {
+    message: string;
+}
 
-const SSG: NextPage<SSGProps> = () => {
+const SSG: NextPage<SSGProps> = (props) => {
+    const { message } = props
+
     return (
         <div>
-            <head>
+            <Head>
                 <title>Static Site Generation</title>
                 <link rel="icon" href="/favicon.ico" />
-            </head>
+            </Head>
             <main>
                 <p>
                     このページは静的サイト生成によってビルド時に生成されたページです．
                 </p>
+                <p>{message}</p>
             </main>
         </div>
     )
+}
+
+export const getStaticProps: GetStaticProps<SSGProps> = async (context) => {
+    const timestamp = new Date().toLocaleString()
+    const message = `${timestamp}にgetStaticPropsが実行されました`
+    console.log(message)
+    return {
+        props: {
+            message,
+        },
+    }
 }
 
 export default SSG
