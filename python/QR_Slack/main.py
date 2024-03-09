9f7261f8fb8c205ec50690f892700634e8d2ebf8
souta-pqr <boriko0116@gmail.com>
2024-03-09 10:57:46 +0900

    kkffkk

--- python/QR_Slack/main.py
+++ python/QR_Slack/main.py
@@ -1,26 +1,7 @@
-import qrcode
 import cv2
 from pyzbar.pyzbar import decode
 import requests
 import time
-
-# QRコードに埋め込む情報
-names = ["小堀さん", "花川さん", "保科さん", "伊藤さん", "遠藤さん", "久馬さん", "関取さん", "若海さん", "森さん", "櫻井さん", "三谷さん", "大賀さん", "藤江さん"]
-
-for i, name in enumerate(names):
-    # QRコードの生成
-    qr = qrcode.QRCode(
-        version=2,  # QRコードの複雑さを増やす
-        error_correction=qrcode.constants.ERROR_CORRECT_M,  # エラー訂正レベルを上げる
-        box_size=10,
-        border=4,
-    )
-    qr.add_data(str(i+1))  # 数字を文字列として埋め込む
-    qr.make(fit=True)
-
-    # QRコードの描画と保存
-    img = qr.make_image(fill='black', back_color='white')
-    img.save(f'qrcode_{name}.png')
 
 # SlackのWebhook URL
 webhook_url = ''
@@ -29,6 +10,9 @@ webhook_url = ''
 cap = cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)  # カメラの解像度を上げる
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)  # カメラの解像度を上げる
+
+# QRコードに埋め込む情報
+names = ["小堀さん", "花川さん", "保科さん", "伊藤さん", "遠藤さん", "久馬さん", "関取さん", "若海さん", "森さん", "櫻井さん", "三谷さん", "大賀さん", "藤江さん"]
 
 # 最後に認証された時間を記録する辞書
 last_auth = {name: 0 for name in names}
@@ -66,3 +50,4 @@ while True:
 # キャプチャをリリースしてウィンドウを閉じる
 cap.release()
 cv2.destroyAllWindows()
+
