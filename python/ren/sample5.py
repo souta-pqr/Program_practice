205389a3271313ae20fe8591356de4a51087f52e
souta-pqr <boriko0116@gmail.com>
2024-08-07 22:57:37 +0900

    python3

---
+++ python/ren/sample5.py
@@ -0,0 +1,12 @@
+code_point = ord('a')
+oct_code = oct(code_point)
+hex_code = hex(code_point)
+print(f"code point: {code_point}")
+print(f"octal: {oct_code}")
+
+print('\141')
+print('\x61')
+
+# Output:
+# code point: 97
+# octal: 0o141
---
+++ python/ren/sample6.py
@@ -0,0 +1,21 @@
+def add(a, b):
+    '''
+    add(a, b)
+   
+    aとbを加算した結果を返す。
+   
+    Parameters
+    ----------
+    a : int
+        加算対象その1
+    b : int
+        加算対象その2
+   
+    Returns
+    ----------
+    int
+        aとbの加算結果
+    '''
+    return a + b
+
+help(add)
